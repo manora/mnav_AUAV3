b3714aff47070accd0aa382686447015f40f8d3c
manora <manora@post.bgu.ac.il>
2015-01-10 00:42:23 +0200

    Release 0 ========= HeartBeat @ 2Hz RawIMU @ 50Hz No TX block

--- clib/MavlinkComm.c
+++ clib/MavlinkComm.c
@@ -2,6 +2,7 @@
 #include "MavlinkComm.h"
 #include "inttypes.h"
 #include <stdio.h>
+#include "AUAV_V3_TestMavlink.h"
 uint8_t UartOutBuff[MAVLINK_MAX_PACKET_LEN];
 
 
@@ -41,79 +42,33 @@ uint16_t PackHeartBeat(uint8_t system_id, uint8_t component_id){
   return(bytes2Send);
 }
 
+uint16_t PackTextMsg(uint8_t system_id, uint8_t component_id){
+  mavlink_message_t msg;
+  mavlink_system_t mavlink_system;
+  mavlink_system.sysid = system_id;                   ///< ID 20 for this airplane
+  mavlink_system.compid = component_id;//MAV_COMP_ID_IMU;     ///< The component sending the message is the IMU, it could be also a Linux process
+  char vr_message[50];
+  memset(vr_message, 0, sizeof (vr_message));
+  sprintf(vr_message, "Hello World");
+  mavlink_msg_statustext_pack(mavlink_system.sysid,
+  mavlink_system.compid,
+  &msg,
+  0,
+  vr_message);
+  return(mavlink_msg_to_send_buffer(UartOutBuff, &msg));
+}
 
-//#include "AUAV_V3_TestMavLink.h"
-//extern MCHP_UART1_TxStr MCHP_UART1_Tx;
 
 uint16_t PackRawIMU(uint8_t system_id, uint8_t component_id, mavlink_raw_imu_t mlRawIMUData ,uint32_t time_usec){
   mavlink_system_t mavlink_system;
 
   mavlink_system.sysid = system_id;                   ///< ID 20 for this airplane
   mavlink_system.compid = component_id;//MAV_COMP_ID_IMU;     ///< The component sending the message is the IMU, it could be also a Linux process
-  mavlink_system.type = MAV_TYPE_FIXED_WING;   ///< This system is an airplane / fixed wing
-
-  // Define the system type, in this case an airplane
-  uint8_t system_type = MAV_TYPE_FIXED_WING;
-  uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
-
-  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
-  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
-  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
-
   //////////////////////////////////////////////////////////////////////////
   mavlink_message_t msg;
-    uint16_t bytes2Send = 0;
-    static uint8_t SendThis = 1;
-    static uint8_t SendScdCounter = 0;
-    char vr_message[50];
-//    if (SendScdCounter == 0)
-//      SendThis = 0;
-//    else
-//      SendThis = 1;
-    switch(SendThis){
-      case 0:
-        // clear the msg to pack a new variable
-        memset(&msg, 0, sizeof (mavlink_message_t));
-        // Pack the Heartbeat message
-        mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, system_type, autopilot_type, system_mode, custom_mode, system_state);
-        /*
-        mavlink_msg_heartbeat_pack(system_id,
-                        component_id,
-                        &msg,
-                        MAV_TYPE_FIXED_WING,//MAV_TYPE_GENERIC,
-                        MAV_AUTOPILOT_SLUGS,//MAV_AUTOPILOT_GENERIC,
-                        MAV_MODE_PREFLIGHT,//mlHeartbeatLocal.base_mode,
-                        MAV_MODE_PREFLIGHT,//mlHeartbeatLocal.custom_mode,
-                        MAV_STATE_UNINIT//mlHeartbeatLocal.system_status
-                        );
-*/
-        // Copy the message to the send buffer
-        bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
-        SendThis = 1;
-        break;
-      case 1:
-        memset(&msg, 0, sizeof (mavlink_message_t));
-        mavlink_msg_raw_imu_pack(mavlink_system.sysid, mavlink_system.compid, &msg , time_usec , mlRawIMUData.xacc , mlRawIMUData.yacc , mlRawIMUData.zacc , mlRawIMUData.xgyro , mlRawIMUData.ygyro , mlRawIMUData.zgyro , mlRawIMUData.xmag , mlRawIMUData.ymag , mlRawIMUData.zmag );
-        bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
-        SendThis = 1;
-        break;
-      case 2:
-        memset(vr_message, 0, sizeof (vr_message));
-        sprintf(vr_message, "Hello World");
-        mavlink_msg_statustext_pack(mavlink_system.sysid,
-        mavlink_system.compid,
-        &msg,
-        0,
-        vr_message);
-        bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
-        SendThis = 0;
-        break;
-
-    }
-    SendScdCounter++;
-    SendScdCounter %= 50;
-    return(bytes2Send);
-
+  memset(&msg, 0, sizeof (mavlink_message_t));
+  mavlink_msg_raw_imu_pack(mavlink_system.sysid, mavlink_system.compid, &msg , time_usec , mlRawIMUData.xacc , mlRawIMUData.yacc , mlRawIMUData.zacc , mlRawIMUData.xgyro , mlRawIMUData.ygyro , mlRawIMUData.zgyro , mlRawIMUData.xmag , mlRawIMUData.ymag , mlRawIMUData.zmag );
+  return( mavlink_msg_to_send_buffer(UartOutBuff, &msg));
 }
 
 char sendQGCDebugMessage(const char * dbgMessage, char severity, unsigned char* bytesToAdd, char positionStart) {
@@ -134,4 +89,21 @@ char sendQGCDebugMessage(const char * dbgMessage, char severity, unsigned char* 
 
 uint8_t GetCharAtBuffIdx(int32_t idx){
     return(UartOutBuff[idx]);
+}
+/* Declare UART1 Tx Circular Buffer Structure */
+extern MCHP_UART1_TxStr MCHP_UART1_Tx;
+
+void TxN_Data_OverU1(uint16_t N){
+  uint16_T i;
+  for (i = 0U; i < N; i++) {
+    uint16_T Tmp;
+    Tmp = ~(MCHP_UART1_Tx.tail - MCHP_UART1_Tx.head);
+    Tmp = Tmp & (Tx_BUFF_SIZE_Uart1 - 1);/* Modulo Buffer Size */
+    if (Tmp != 0) {
+      MCHP_UART1_Tx.buffer[MCHP_UART1_Tx.tail] = UartOutBuff[i];
+      MCHP_UART1_Tx.tail = (MCHP_UART1_Tx.tail + 1) & (Tx_BUFF_SIZE_Uart1 - 1);
+      Tmp--;
+    }
+  }
+  _U1TXIF = U1STAbits.TRMT;
 }
