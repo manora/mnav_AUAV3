48b9875dc7f88e6d6e7a4cc0957c1dd7faa12623
manora <manora@post.bgu.ac.il>
2015-04-25 01:30:07 +0300

    Basic Mavlink RX Implemented and tested Heartbeat and Parameters Interface

--- clib/MavlinkComm.c
+++ clib/MavlinkComm.c
@@ -6,9 +6,279 @@
 #include <stdio.h>
 #include "AUAV_V3_TestMavlink.h"
 uint8_t UartOutBuff[MAVLINK_MAX_PACKET_LEN];
+struct CircBuffer comMavlinkBuffer;
+CBRef uartMavlinkInBuffer;
+uint8_T DatafromGSmavlink[MAXINLEN+2];
+mavlink_pending_requests_t mlPending;
+mavlink_heartbeat_t mlHeartbeat;
+
+struct pi_struct mlParamInterface;
+
+void uartMavlinkBufferInit (void){
+  _U1RXIP = 1;                         /* Rx Interrupt priority set to 1 */
+  _U1RXIF = 0;
+  _U1RXIE = 1;                         /* Enable Interrupt */
+  /* Configure Remappables Pins */
+   RPINR18 = 0x62;
+
+  uartMavlinkInBuffer = (struct CircBuffer*) &comMavlinkBuffer;
+  newCircBuffer(uartMavlinkInBuffer);
+}
+
+void __attribute__((__interrupt__,__auto_psv__)) _U1RXInterrupt(void)
+{
+  //
+        _U1RXIF = 0;
+    // Read the buffer while it has data
+    // and add it to the circular buffer
+    while (U1STAbits.URXDA == 1) {
+        writeBack(uartMavlinkInBuffer, (unsigned char) U1RXREG);
+    }
+
+    // If there was an overun error clear it and continue
+    if (U1STAbits.OERR == 1) {
+        _U1RXIF = 0;
+        U1STAbits.OERR = 0;
+    }
+
+    // clear the interrupt
+    IFS0bits.U1RXIF = 0;
+
+}
+
+uint8_t isFinite(float s) {
+  // By IEEE 754 rule, 2*Inf equals Inf
+  return ((s == s) && ((s == 0) || (s != 2*s)));
+}
+
+void InitParameterInterface(void)
+{
+    strcpy(mlParamInterface.param_name[PAR_PID_AIRSPEED_P], "PID_AIRSPD_P");
+    strcpy(mlParamInterface.param_name[PAR_PID_AIRSPEED_I], "PID_AIRSPD_I");
+    strcpy(mlParamInterface.param_name[PAR_PID_AIRSPEED_D], "PID_AIRSPD_D");
+
+    strcpy(mlParamInterface.param_name[PAR_PID_PITCH_FO_P], "PID_PIT_FO_P");
+    strcpy(mlParamInterface.param_name[PAR_PID_PITCH_FO_I], "PID_PIT_FO_I");
+    strcpy(mlParamInterface.param_name[PAR_PID_PITCH_FO_D], "PID_PIT_FO_D");
+
+    strcpy(mlParamInterface.param_name[PAR_PID_ROLL_CON_P], "PID_ROLL_CO_P");
+    strcpy(mlParamInterface.param_name[PAR_PID_ROLL_CON_I], "PID_ROLL_CO_I");
+    strcpy(mlParamInterface.param_name[PAR_PID_ROLL_CON_D], "PID_ROLL_CO_D");
+
+    strcpy(mlParamInterface.param_name[PAR_PID_HE_TO_PI_P], "PID_HE2PITC_P");
+    strcpy(mlParamInterface.param_name[PAR_PID_HE_TO_PI_I], "PID_HE2PITC_I");
+
+    strcpy(mlParamInterface.param_name[PAR_PID_HEI_ERR_FF], "PID_HERR_FF");
+
+    strcpy(mlParamInterface.param_name[PAR_PID_YAW_DAMP_P], "PID_YAW_DA_P");
+    strcpy(mlParamInterface.param_name[PAR_PID_YAW_DAMP_I], "PID_YAW_DA_I");
+    strcpy(mlParamInterface.param_name[PAR_PID_YAW_DAMP_D], "PID_YAW_DA_D");
+
+}
+
+void protDecodeMavlink(void) {
+
+    uint8_t indx, writeSuccess, commChannel = 1;
+    mavlink_param_set_t set;
+    mavlink_message_t msg;
+    mavlink_status_t status;
+    uint8_t* dataIn;
+    // fix the data length so if the interrupt adds data
+    // during execution of this block, it will be read
+    // until the next gsRead
+    unsigned int tmpLen = getLength(uartMavlinkInBuffer), i = 0;
+
+    // if the buffer has more data than the max size, set it to max,
+    // otherwise set it to the length
+    //DatafromGSmavlink[0] = (tmpLen > MAXINLEN) ? MAXINLEN : tmpLen;
+
+    // read the data
+    //for (i = 1; i <= DatafromGSmavlink[0]; i += 1) {
+      //mavlink_parse_char(commChannel, readFront(uartBufferInMavlink), &msg, &status);
+      //DatafromGSmavlink[i] = readFront(uartMavlinkInBuffer);
+    //}
+    //dataIn = DatafromGSmavlink;
+    // increment the age of heartbeat
+    mlPending.heartbeatAge++;
+
+    for (i = 0; i <= tmpLen; i++) {
+        // Try to get a new message
+       if (mavlink_parse_char(commChannel, readFront(uartMavlinkInBuffer), &msg, &status)) {
+                    // Handle message
+            switch (msg.msgid) {
+                case MAVLINK_MSG_ID_HEARTBEAT:
+                    mavlink_msg_heartbeat_decode(&msg, &mlHeartbeat);
+                    // Reset the heartbeat
+                    mlPending.heartbeatAge = 0;
+                    break;
+                case MAVLINK_MSG_ID_PARAM_REQUEST_LIST:
+                    mlPending.piTransaction = 1;
+                    mlPending.piProtState = PI_SEND_ALL_PARAM;
+                    mlPending.piCurrentParamInTransaction = 0;
+                    break;
+
+                case MAVLINK_MSG_ID_PARAM_REQUEST_READ:
+                    // If it was in the middle of a list transmission or there is already a param enqueued
+                    mlPending.piTransaction = 1;
+                    switch (mlPending.piProtState) {
+                        case PI_IDLE:
+                            mlPending.piBackToList = 0; // no need to go back
+                            mlPending.piQIdx = -1; // no Index
+                            mlPending.piCurrentParamInTransaction = mavlink_msg_param_request_read_get_param_index(&msg); // assign directly
+                            mlPending.piProtState = PI_SEND_ONE_PARAM;
+                            break;
+
+                        case PI_SEND_ALL_PARAM:
+                            mlPending.piBackToList = 1; // mark to go back
+                            mlPending.piQIdx++; // done like this because when empty index = -1
+                            mlPending.piQueue[mlPending.piQIdx] = mavlink_msg_param_request_read_get_param_index(&msg); // put in in queue
+                            mlPending.piProtState = PI_SEND_ONE_PARAM;
+                            break;
+
+                        case PI_SEND_ONE_PARAM:
+                            if (mlPending.piBackToList) {
+                                mlPending.piQIdx++; // done like this because when empty index = -1
+                                mlPending.piQueue[mlPending.piQIdx] = mavlink_msg_param_request_read_get_param_index(&msg); // put in in queue
+                            }
+                            mlPending.piProtState = PI_SEND_ONE_PARAM;
+                            break;
+                    }
+                    break;
+
+                case MAVLINK_MSG_ID_PARAM_SET:
+                    mavlink_msg_param_set_decode(&msg, &set);
+
+                    if ((uint8_t) set.target_system == (uint8_t) SYSTEMID &&
+                        (uint8_t) set.target_component == (uint8_t) COMPID) {
 
 
+                        char* key = (char*) set.param_id;
+                        uint8_t i, j;
+                        uint8_t match;
+                        for (i = 0; i < PAR_PARAM_COUNT; i++) {
+                            match = 1;
+                            for (j = 0; j < PARAM_NAME_LENGTH; j++) {
+                                // Compare
+                                if (((char) (mlParamInterface.param_name[i][j]))
+                                    != (char) (key[j])) {
+                                    match = 0;
+                                } // if
 
+                                // End matching if null termination is reached
+                                if (((char) mlParamInterface.param_name[i][j]) == '\0') {
+                                    break;
+                                } // if
+                            }// for j
+
+                            // Check if matched
+                            if (match) {
+                                //sw_debug = 1;
+                                // Only write and emit changes if there is actually a difference
+                                // AND only write if new value is NOT "not-a-number"
+                                // AND is NOT infinity
+
+                                if (isFinite(set.param_value)) {
+
+                                    mlParamInterface.param[i] = set.param_value;
+
+                                    // Report back new value
+                                    mlPending.piBackToList = 0; // no need to go back
+                                    mlPending.piQIdx = -1; // no Index
+                                    mlPending.piCurrentParamInTransaction = i; // assign directly
+                                    mlPending.piProtState = PI_SEND_ONE_PARAM;
+                                    mlPending.piTransaction = 1;
+
+                                } // if different and not nan and not inf
+                            } // if match
+                        }// for i
+                    } // if addressed to this
+                    break;
+              default:
+                break;
+            }
+       }
+    }
+}
+
+uint16_t ParameterInterfaceResponse(uint8_t system_id, uint8_t component_id){
+  mavlink_message_t msg;
+  uint16_t bytes2Send = 0;
+  if (mlPending.piTransaction){
+
+    switch (mlPending.piProtState) {
+      case PI_SEND_ALL_PARAM:
+          if (mlPending.piCurrentParamInTransaction < PAR_PARAM_COUNT) {
+
+              mavlink_msg_param_value_pack(system_id,
+                  component_id,
+                  &msg,
+                  mlParamInterface.param_name[mlPending.piCurrentParamInTransaction],
+                  mlParamInterface.param[mlPending.piCurrentParamInTransaction],
+                  MAV_PARAM_TYPE_REAL32, // TODO: make sure this is correct later on
+                  PAR_PARAM_COUNT,
+                  mlPending.piCurrentParamInTransaction);
+
+              mlPending.piCurrentParamInTransaction++;
+
+              // Copy the message to the send buffer
+              bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
+
+          } else {
+              mlPending.piProtState = PI_IDLE;
+              mlPending.piCurrentParamInTransaction = PAR_PARAM_COUNT;
+              mlPending.piTransaction = 0;
+              mlPending.piBackToList = 0;
+              mlPending.piQIdx = -1;
+          }
+          break;
+
+      case PI_SEND_ONE_PARAM:
+
+
+          if (!mlPending.piBackToList) { // if this is just a single pm read, i.e. not a retransmission
+              mavlink_msg_param_value_pack(system_id,
+                  component_id,
+                  &msg,
+                  mlParamInterface.param_name[mlPending.piCurrentParamInTransaction],
+                  mlParamInterface.param[mlPending.piCurrentParamInTransaction],
+                  MAV_PARAM_TYPE_REAL32,
+                  PAR_PARAM_COUNT,
+                  mlPending.piCurrentParamInTransaction);
+
+              // Copy the message to the send buffer
+              bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
+
+
+              mlPending.piCurrentParamInTransaction = PAR_PARAM_COUNT;
+              mlPending.piProtState = PI_IDLE;
+              mlPending.piTransaction = 0;
+          } else { // you will need to go back to send all
+
+              if (mlPending.piQIdx < 0) { // if you've sent all the requests, then go back to list
+                  mlPending.piProtState = PI_SEND_ALL_PARAM;
+                  mlPending.piBackToList = 0;
+
+              } else { // send the requests
+                  mavlink_msg_param_value_pack(system_id,
+                      component_id,
+                      &msg,
+                      mlParamInterface.param_name[mlPending.piQueue[mlPending.piQIdx]],
+                      mlParamInterface.param[mlPending.piQueue[mlPending.piQIdx]],
+                      MAV_PARAM_TYPE_REAL32,
+                      PAR_PARAM_COUNT,
+                      mlPending.piQueue[mlPending.piQIdx]);
+
+                  // Copy the message to the send buffer
+                  bytes2Send += mavlink_msg_to_send_buffer(UartOutBuff, &msg);
+
+                  // decrement the queue index
+                  mlPending.piQIdx--;
+              } // QIdx < 0
+          }// !backToList
+    }//switch
+  }//if (mlPending.piTransaction)
+  return(bytes2Send);
+}
 uint16_t PackHeartBeat(uint8_t system_id, uint8_t component_id){
   mavlink_system_t mavlink_system;
 
--- clib/MavlinkComm.h
+++ clib/MavlinkComm.h
@@ -1,5 +1,124 @@
 #ifndef  _MAVLINK_COMM_H_
 #define  _MAVLINK_COMM_H_
+#include "circBuffer.h"
+
+extern CBRef uartMavlinkInBuffer;
+extern struct CircBuffer comMavlinkBuffer;
+void uartMavlinkBufferInit (void);
+  #define SYSTEMID		101
+  #define COMPID		1
+
+  #define MAXINLEN      200
+  #define MAXSEND	109
+  #define MAX_NUM_WPS   17
+  #define PARAM_NAME_LENGTH	16
+  enum WP_PROTOCOL {
+    WP_PROT_IDLE,
+    WP_PROT_LIST_REQUESTED,
+    WP_PROT_NUM_SENT,
+    WP_PROT_TX_WP,
+    WP_PROT_RX_WP,
+    WP_PROT_SENDING_WP_IDLE,
+    WP_PROT_GETTING_WP_IDLE
+  };
+
+  enum PARAM_INTERFACE {
+    PI_IDLE,
+    PI_SEND_ALL_PARAM,
+    PI_SEND_ONE_PARAM
+  };
+
+  enum PARAM_INTERFACE_IDX {
+    PAR_PID_AIRSPEED_P = 0,
+    PAR_PID_AIRSPEED_I = 1,
+    PAR_PID_AIRSPEED_D = 2,
+
+    PAR_PID_PITCH_FO_P = 3,
+    PAR_PID_PITCH_FO_I = 4,
+    PAR_PID_PITCH_FO_D = 5,
+
+    PAR_PID_ROLL_CON_P = 6,
+    PAR_PID_ROLL_CON_I = 7,
+    PAR_PID_ROLL_CON_D = 8,
+
+    PAR_PID_HE_TO_PI_P = 9,
+    PAR_PID_HE_TO_PI_I = 10,
+
+    PAR_PID_HEI_ERR_FF = 11,
+
+    PAR_PID_YAW_DAMP_P = 12,
+    PAR_PID_YAW_DAMP_I = 13,
+    PAR_PID_YAW_DAMP_D = 14,
+
+
+    PAR_PARAM_COUNT // Always at the end, do not assign value
+  };
+  
+  struct pi_struct {
+    float param[PAR_PARAM_COUNT];
+    char param_name[PAR_PARAM_COUNT][PARAM_NAME_LENGTH];
+  };
+
+   typedef struct mavlink_pending_requests_t {
+        // requests
+        uint8_t ping;
+        uint8_t midLvlCmds;
+        uint8_t pt;
+        uint8_t isrLoc;
+
+        // Acknowledgement counters
+        uint8_t commandAck;
+        uint8_t wpAck;
+
+        // Stuff to send
+        uint8_t statustext;
+        uint8_t command;
+
+        // slugs Acknowledge
+        // uint8_t slugsAction; // Note: migrate slugs actions into commands, acks, and text
+
+        // WP Protocol states
+        uint8_t wpTransaction;
+        uint8_t wpProtState;
+        uint8_t wpCurrentWpInTransaction;
+        uint8_t wpTimeOut;
+        uint8_t wpTotalWps;
+        uint8_t wpSendCurrent; // send current mission item
+
+        // Info
+        uint8_t pidIdx;
+        uint8_t wpsIdx;
+
+        //uint8_t 	requestCount;
+
+        // Parameter Interface
+        uint8_t piTransaction;
+        uint8_t piProtState;
+        uint8_t piCurrentParamInTransaction;
+        uint8_t piBackToList;
+        uint8_t piQueue[5];
+        int8_t piQIdx;
+
+        // spi
+        uint8_t spiToSensor[MAXSEND];
+        uint8_t spiCurrentIndex;
+        uint8_t spiTotalData;
+        uint8_t spiSendGSLocation;
+
+        // Heartbeat status
+        uint16_t heartbeatAge;
+
+    } mavlink_pending_requests_t;
+
+    typedef struct mavlink_mission_item_values_t {
+        float lat[MAX_NUM_WPS];
+        float lon[MAX_NUM_WPS];
+        float alt[MAX_NUM_WPS];
+        uint8_t type[MAX_NUM_WPS];
+        uint16_t orbit[MAX_NUM_WPS];
+        uint8_t wpCount;
+    } mavlink_mission_item_values_t;
+
 uint16_t PackRawIMU(uint8_t system_id, uint8_t component_id, mavlink_raw_imu_t mlRawIMUData ,uint32_t time_usec);
 char sendQGCDebugMessage(const char * dbgMessage, char severity, unsigned char* bytesToAdd, char positionStart) ;
 //uint8_t GetCharAtIdx(int2_t idx);
